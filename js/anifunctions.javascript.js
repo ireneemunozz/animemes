23b3c04701973eeca3bc96ba8301787029f9d587
irene <irenelittlecrocodile@gmail.com>
2019-12-27 12:54:32 +0100

    primer cambio

---
+++ js/anifunctions.javascript.js
@@ -0,0 +1,10 @@
+
+
+var fecha = new Date();
+
+if (fecha.getHours()<12) {
+    alert("Buenos días, bienvenidos a nuestra nueva página!");
+} else {
+    alert("Buenas tardes, bienvenidos a nuestra nueva página!");
+}
+
